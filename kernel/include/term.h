8a2b0d584246b8985ee5edeade88c9d1d71732c3
Andrea Orrù <andrew.iwakura@gmail.com>
2013-12-31 16:52:47 +0100

    Syscall library headers.

--- kernel/include/term.h
+++ kernel/include/term.h
@@ -1,20 +1,20 @@
 #pragma once
 #include "x86.h"
 
-#define ALERT(message...)  \
+#define ALERT(message...) \
     printf("\n\e0415" message)
 
-#define ERROR(message...)  \
-({                         \
-    ALERT(message);        \
-    hlt();                 \
+#define ERROR(message...) \
+({                        \
+    ALERT(message);       \
+    hlt();                \
 })
 
-#define STEP(function, message...)      \
-({                                      \
-    printf("\eF09  >> \eF15" message);  \
-    function;                           \
-    printf("\eF10 OK!\n");              \
+#define STEP(function, message...)     \
+({                                     \
+    printf("\eF09  >> \eF15" message); \
+    function;                          \
+    printf("\eF10 OK!\n");             \
 })
 
 void put(char c);
