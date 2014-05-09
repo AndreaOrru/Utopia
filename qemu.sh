d99a3aae8e137fa0bec5f90f6963dd26c0199ba2
Andrea Orrù <andrew.iwakura@gmail.com>
2014-05-09 20:58:53 +0200

    i686-gdb.

--- qemu.sh
+++ qemu.sh
@@ -5,7 +5,7 @@ SERVERS="servers/test/test.elf,servers/test2/test2.elf"
 if [ "$1" = "-g" ]; then
     qemu-system-i386 -s -S -kernel kernel/kernel.elf -initrd $SERVERS &
     sleep 0.5
-    i586-elf-gdb
+    i686-elf-gdb
 elif [ "$1" = "-w" ]; then
     qemu-system-i386 -s -S -kernel kernel/kernel.elf -initrd $SERVERS &
 else
